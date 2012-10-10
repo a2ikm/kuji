e65d8fc519c5a1e97345c88876fbb36f92bb39d5
Masato Ikeda <masato.ikeda@gmail.com>
2012-10-10 17:56:05 +0900

    引用をつけたり、レイアウトを変更したり

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
-# code: utf-8
+# coding: utf-8
 require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/' do
+    @quote = [
+      "チェシャ猫「どっちがいいかわからないときはどっちも一緒だよ」", 
+      "隣の席の人「難しく考えすぎなんですよ( ｰ`дｰ´)ｷﾘｯ」", 
+      "ごろー「まぁ、自分の信じる道を突き進めばいいんじゃないかなぁ」"
+    ].sample
     slim :index
   end
 
